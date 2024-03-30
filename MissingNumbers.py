81a4354b9d7f013fd9c2fd47c24a7a697f2af733
Azima Qadirli <131367409+Azima-Qadirli@users.noreply.github.com>
2024-03-30 12:03:11 +0400

    Update MissingNumbers.py

--- MissingNumbers.py
+++ MissingNumbers.py
@@ -1,3 +1,10 @@
 class Solution:
     def missingNumber(self, nums:int) -> int:
         return len(nums) * (len(nums) + 1) // 2 - sum(nums)
+    #or
+    class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length=len(nums)
+        total=length*(length+1)//2
+        return total-sum(nums)
+        
