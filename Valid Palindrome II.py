8fd2c72542f9fed0b9c2774400de458363d4e6c0
Azima Qadirli <131367409+Azima-Qadirli@users.noreply.github.com>
2024-03-30 14:05:02 +0400

    Update Valid Palindrome II.py

--- Valid Palindrome II.py
+++ Valid Palindrome II.py
@@ -1,8 +1,9 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        left,right=0, len(s)-1
+        left, right = 0, len(s) - 1
         while left < right:
-            if s[left] == s[right]:
-                left +=1
-                right -=1
-        return True
+            if s[left] != s[right]:
+                return False  # If characters don't match, it's not a palindrome.
+            left += 1
+            right -= 1
+        return True  # If the loop completes, it's a palindrome.
